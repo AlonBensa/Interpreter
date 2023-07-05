93064b412ede08cae3b0a73004fa7a91ed034896
avivs22 <avivsking10@gmail.com>
2023-07-05 23:39:58 +0300

    Finish Exceptions

--- TheInterpreter/TheInterpreter/IndexError.h
+++ TheInterpreter/TheInterpreter/IndexError.h
@@ -4,6 +4,9 @@
 class IndexError : public InterpreterExceptions
 {
 public:
+	IndexError(std::string functionName);
 	virtual const char* what() const noexcept;
+private:
+	std::string _functionName;
 };
 
