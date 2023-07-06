d2f121f6785206368fabf110d88faf16b1953fb0
avivs22 <avivsking10@gmail.com>
2023-07-06 22:31:06 +0300

    Helper adds

--- TheInterpreter/TheInterpreter/Helper.cpp
+++ TheInterpreter/TheInterpreter/Helper.cpp
@@ -19,3 +19,34 @@ int Helper::getIndex(int length, int index)
 	}
 	return length + index;
 }
+
+int Helper::FindSizeArray(Type* type) 
+{
+    int count = 0;
+    while (dynamic_cast<Sequence*>(type)) 
+    {
+       
+    }
+}
+
+Type* Helper::FindSequence(Sequence* seq)
+{
+    if (dynamic_cast<Stack*>(seq)) {
+        return ((Stack*)seq)->pop();
+    }
+    if (dynamic_cast<Queue*>(seq)) {
+        return ((Queue*)seq)->dequeue();
+    }
+    if (dynamic_cast<List*>(seq)) {
+        return ((List*)seq)->pop();
+    }
+    if (dynamic_cast<Dictionary*>(seq)) {
+        return ((Dictionary*)(seq))->popItem().first;
+    }
+    if (dynamic_cast<String*>(seq)) {
+        return nullptr;
+    }
+    if (dynamic_cast<Tuple*>(seq)) {
+        return ((Tuple*)(seq))[0];
+    }
+}
--- TheInterpreter/TheInterpreter/Helper.h
+++ TheInterpreter/TheInterpreter/Helper.h
@@ -1,10 +1,20 @@
 #pragma once
 #include <string>
+#include "Type.h"
+#include "Sequence.h"
+#include "Stack.h"
+#include "Queue.h"
+#include "List.h"
+#include "Tuple.h"
+#include "Dictionary.h"
+#include "String.h"
 
 class Helper
 {
 public:
 	static bool isInt(std::string str);
 	static int getIndex(int length,int index);
+	int FindSizeArray(Type* type);
+	Type* FindSequence(Sequence* seq);
 };
 
