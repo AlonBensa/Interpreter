5580674ba1fb5fc74d5a2ae6bee09f8ca96df9cb
avivs22 <avivsking10@gmail.com>
2023-07-06 22:06:46 +0300

    Forgot save header

--- TheInterpreter/TheInterpreter/TypeError.h
+++ TheInterpreter/TheInterpreter/TypeError.h
@@ -6,6 +6,7 @@ public:
 	TypeError(Type* firstType, Type* secondType, std::string typesOperator, std::string objName);
 	TypeError(std::string objName,std::string functionName,int amountOfArgumentGiven, int amountOfArgumentNeeded);
 	TypeError(std::string functionName, std::string typeGiven, std::string typeNeeded, std::string argNum);
+	TypeError(std::string wrongType);
 	virtual const char* what() const noexcept;
 private:
 	std::string _typesOperator;
@@ -18,5 +19,6 @@ private:
 	int _amountOfArgumentGiven;
 	int _amountOfArgumentNeeded;
 	std::string _argNum;
+	std::string _wrongType;
 };
 
