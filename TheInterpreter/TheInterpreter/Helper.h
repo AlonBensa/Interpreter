7f6f35213a00bf1bf124fbed8095f4548a24ea56
avivs22 <avivsking10@gmail.com>
2023-07-20 18:08:46 +0300

    FindBasicType Function to help in InOperator

--- TheInterpreter/TheInterpreter/Helper.h
+++ TheInterpreter/TheInterpreter/Helper.h
@@ -8,11 +8,16 @@
 #include "Tuple.h"
 #include "Dictionary.h"
 #include "String.h"
+#include "Float.h"
+#include "Integer.h"
+#include "Boolean.h"
+#include "Void.h"
 
 class Helper
 {
 public:
 	static bool isInt(std::string str);
 	static int getIndex(int length,int index);
+	static std::string FindBasicType(Type* type);
 };
 
